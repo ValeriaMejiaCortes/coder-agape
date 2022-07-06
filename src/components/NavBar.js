02b1413efa15bbc25784b6778f1ab6d4983f8482
ValeriaMejiaCortes <valemejiacortez@gmail.com>
2022-07-06 18:06:00 -0500

    change_navbar_ecommerce_mejia

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,24 @@
-import logo from '../assets/logo.jpg';
+import logo from '../assets/logo01.png';
+import './NavBar.css';
 
 const NavBar = () => {
     return(
       <>
-        <nav className="navbar navbar-expand-lg" style={{"backgroundColor": "black"}}>
+        <nav className="navbar navbar-expand-lg" style={{"backgroundColor": "#5A3A1C"}}>
             <div className="container-fluid">
-                <a className="navbar-brand" href="/#" padding="0">
-                    <img src={logo} alt="" width="80" height="80"></img>
+                <a className="navbar-brand a_logo" href="/#" padding="0">
+                    <img src={logo} alt="" width="100" height="100"></img>
                 </a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div className="navbar-nav">
-                    <a className="nav-link active item_menu" aria-current="page" href="/#">Tablas</a>
-                    <a className="nav-link item_menu" href="/#">Sangrías</a>
-                    <a className="nav-link item_menu" href="/#">Kids</a>
-                    <a className="nav-link item_menu" href="/#">Picnics</a>
-                </div>
+                <div className="collapse navbar-collapse items" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                        <a className="nav-link active item_menu" aria-current="page" href="/#">Tablas</a>
+                        <a className="nav-link item_menu" href="/#">Sangrías</a>
+                        <a className="nav-link item_menu" href="/#">Kids</a>
+                        <a className="nav-link item_menu" href="/#">Picnics</a>
+                    </div>
                 </div>
             </div>
         </nav>
